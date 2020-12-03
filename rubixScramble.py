5ebc58db19959b8a60118a93d604d43d32077a34
etreadway <68955513+etreadway@users.noreply.github.com>
2020-12-03 10:54:00 -0600

    Update rubixScramble.py
    Switched the order of prime and side/renamed direc to prime

--- rubixScramble.py
+++ rubixScramble.py
@@ -2,14 +2,13 @@
 
 import random
 
-moves = ''
 side = ['F', 'R', 'U', "B", 'L', 'D']
-direc = [" ","'" ]
+prime = [" ","'" ]
 
-
+moves = ''
 while len(moves) < 100:
-    moves += str(direc[random.randint(0, 1)])
     moves += str(side[random.randint(0, 5)])
+    moves += str(prime[random.randint(0, 1)])
     moves += ', '
 
 
